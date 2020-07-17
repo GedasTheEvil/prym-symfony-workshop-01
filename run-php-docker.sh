a6a62b1b39423f3aef535b2a933ae3bf9a166c07
Gediminas Skucas <gediminas.skucas@gmail.com>
2020-07-17 08:58:15 +0300

    Update run-php-docker.sh

--- run-php-docker.sh
+++ run-php-docker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 APP=$@
-PORT_9000=8069
 
 if [[ "${APP}" == "" ]]; then
 	APP="/bin/bash"
@@ -9,6 +8,5 @@ fi
 
 docker run --rm -it --name prym-symfony-php \
 	-v $PWD:/home/app -w /home/app \
-	-e "PORT=9000" -p ${PORT_9000}:9000  \
 	php:7.4-cli ${APP}
 
